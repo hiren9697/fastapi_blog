955a517c4b9a16438115502607d2c86889cbfd9c
Hiren Fadadu <hirenfaldu9697@gmail.com>
2026-07-07 08:06:15 +0530

    Create static posts API

--- main.py
+++ main.py
@@ -5,3 +5,12 @@ app = FastAPI()
 @app.get("/")
 def home():
     return {"message": "Welcome to the FastAPI Blog! edit"}
+
+posts: list[dict] = [
+    {"id": 1, "author": "Hirenkumar", "title": "Fast API", "content": "This is awsome", "date_posted": "2022"},
+    {"id": 2, "author": "John", "title": "Fast API", "content": "This is awsome", "date_posted": "2022"}
+]
+
+@app.get("/api/posts")
+def get_posts():
+    return posts
