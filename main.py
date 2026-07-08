786317f43079c24692866a2916c3f8a1a6999485
Hiren Fadadu <hirenfaldu9697@gmail.com>
2026-07-08 06:03:44 +0530

    Add Mark comments

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ from schemas import PostResponse
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
-app = FastAPI()
-
-@app.get("/", response_class= HTMLResponse, include_in_schema=False)
-@app.get("/posts", response_class= HTMLResponse, include_in_schema=False)
-def home():
-    return f"<h1>{posts[0]['title']}</h1>"
-
+# MARK: - Mock Data
 posts: list[dict] = [
     {"id": 1, "author": "Hirenkumar", "title": "Fast API", "content": "This is awsome", "date_posted": "2022"},
     {"id": 2, "author": "John", "title": "Fast API", "content": "This is awsome", "date_posted": "2022"}
 ]
+
+app = FastAPI()
+
+# MARK: - API Endpoints
+@app.get("/", response_class= HTMLResponse, include_in_schema=False)
+@app.get("/posts", response_class= HTMLResponse, include_in_schema=False)
+def home():
+    return f"<h1>{posts[0]['title']}</h1>"
 
 @app.get("/api/posts", response_model=list[PostResponse])
 def get_posts():
