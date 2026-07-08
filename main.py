0d89895dccfc45dcd7e837c3330da518070ff145
Hiren Fadadu <hirenfaldu9697@gmail.com>
2026-07-08 06:34:34 +0530

    Added exception handlers

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from fastapi import HTTPException, status
+from fastapi.responses import JSONResponse
+from fastapi import HTTPException, Request, status
 from schemas import PostResponse
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
+from fastapi.exceptions import RequestValidationError
+from starlette.exceptions import HTTPException as StarlettedHTTPException
 
 # MARK: - Mock Data
 posts: list[dict] = [
@@ -26,4 +29,16 @@ def post_page(post_id: int):
     for post in posts: 
         if post['id'] == post_id:
             return post 
-    return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {post_id} not found")
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post with id {post_id} not found")
+
+# MARK: - Exception Handlers
+@app.exception_handler(RequestValidationError)
+def validation_exception_handler(request: Request, exception: RequestValidationError):
+    return JSONResponse(status_code=status.HTTP_422_UNPROCESSABLE_CONTENT, content={"detail": exception.errors()})
+
+@app.exception_handler(StarlettedHTTPException) 
+def general_http_exception_handler(request: Request, exception: StarlettedHTTPException): 
+    message = (exception.detail
+     if exception.detail
+     else "An error occured, please check your request and try again")
+    return JSONResponse(status_code=exception.status_code, content={"detail": message})
